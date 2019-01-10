a807354d007fc3ec9d9fb4c593e97be0e2f9e920
jay <tangzai0709@qq.com>
2019-01-10 12:50:42 +0800

    Array support generics

--- Array/Array.go
+++ Array/Array.go
@@ -1,14 +1,18 @@
 package Array
 
+import (
+	"fmt"
+)
+
 type Array struct {
-	data []int
+	data []interface{}
 	size int
 }
 
 func NewArray(capacity int) *Array {
 	arr := new(Array)
 	arr.size = 0
-	arr.data = make([]int, 0, capacity)
+	arr.data = make([]interface{}, capacity)
 	return arr
 }
 
@@ -24,3 +28,92 @@ func (arr *Array) IsEmpty() bool {
 	return arr.size == 0
 }
 
+func (arr *Array) Prepend(e interface{}) {
+	arr.Insert(0, e)
+}
+
+func (arr *Array) Append(e interface{}) {
+	arr.Insert(arr.size, e)
+}
+
+func (arr *Array) Insert(index int, e interface{})  {
+	if arr.size == cap(arr.data) {
+		panic("Insert Failed, Array is full.")
+	}
+	if index < 0 || index > arr.size {
+		panic("Insert Failed, Out of range Index.")
+	}
+
+	for i := arr.size-1; i >= index; i-- {
+		arr.data[i+1] = arr.data[i]
+	}
+	arr.data[index] = e
+	arr.size++
+}
+
+func (arr *Array) ToString() string {
+	arrInStr := fmt.Sprintf("Array: size = %d, capacity = %d ", arr.size, cap(arr.data))
+	arrInStr = arrInStr + fmt.Sprintf("%v", arr.data[:arr.size])
+	return arrInStr
+}
+
+func (arr *Array) Get(index int) interface{} {
+	if index < 0 || index > arr.size {
+		panic("Get Failed, Index is illegal.")
+	}
+	return arr.data[index]
+}
+
+func (arr *Array) Set(index int, e interface{}) {
+	if index < 0 || index > arr.size {
+		panic("Get Failed, Index is illegal.")
+	}
+	arr.data[index] = e
+}
+
+func (arr *Array) Contains(e interface{}) bool {
+	for i := 0; i < arr.size; i++ {
+		if arr.data[i] == e {
+			return true
+		}
+	}
+	return false
+}
+
+func (arr *Array) Index(e interface{}) int {
+	for i := 0; i < arr.size; i++ {
+		if arr.data[i] == e {
+			return i
+		}
+	}
+	return -1
+}
+
+func (arr *Array) Delete(index int) interface{} {
+	if index < 0 || index >= arr.size {
+		panic("Delete Failed, Index is illegal.")
+	}
+	ret := arr.data[index]
+	for i := index + 1; i < arr.size; i++ {
+		arr.data[i-1] = arr.data[i]
+	}
+	arr.size--
+	return ret
+}
+
+func (arr *Array) Pop() interface{} {
+	return arr.Delete(arr.size-1)
+}
+
+func (arr *Array) UnShift() interface{} {
+	return arr.Delete(0)
+}
+
+func (arr *Array) DeleteElement(e interface{}) bool {
+	index := arr.Index(e)
+	if index > 0 {
+		arr.Delete(index)
+		return true
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -1,8 +1,81 @@
 package main
 
+import (
+	"data_structure/Array"
+	"fmt"
+)
+
 func main() {
 
+	arr := Array.NewArray(20)
+	for i := 0; i < 10; i++ {
+		arr.Append(i)
+	}
+	fmt.Println(arr.ToString())
 
+	arr.Insert(1, 100)
+	fmt.Println(arr.ToString())
 
+	arr.Prepend(99)
+	fmt.Println(arr.ToString())
+
+	arr.Append(200)
+	fmt.Println(arr.ToString())
+
+	arr.Set(4, 101)
+	fmt.Println(arr.ToString())
+
+	fmt.Println("Get Index = 8:", arr.Get(8))
+
+	fmt.Println("Array contains 101 ?", arr.Contains(101))
+	fmt.Println("Array contains 201 ?", arr.Contains(201))
+	fmt.Println("index of 9 =", arr.Index(9))
+
+	fmt.Println("Delete index 5 =", arr.Delete(5))
+	fmt.Println(arr.ToString())
+
+	fmt.Println("Pop: ", arr.Pop())
+	fmt.Println(arr.ToString())
+
+	fmt.Println("UnShift: ", arr.UnShift())
+	fmt.Println(arr.ToString())
+
+	fmt.Println("Delete Element 1:", arr.DeleteElement(1))
+	fmt.Println(arr.ToString())
+
+	fmt.Println()
+	fmt.Println("New String Array:")
+	arrStr := Array.NewArray(10)
+
+	arrStr.Append("php")
+	arrStr.Append("java")
+	arrStr.Append("golang")
+	arrStr.Append("objectc")
+	arrStr.Append("python")
+	arrStr.Append("ruby")
+	arrStr.Append("cpp")
+	arrStr.Append("clang")
+	arrStr.Append("erlang")
+	fmt.Println(arrStr.ToString())
+
+	arrStr.Set(8, "ios")
+	fmt.Println(arrStr.ToString())
+
+	fmt.Println("Get Index = 8:", arrStr.Get(8))
+
+	fmt.Println("Array contains php ?", arrStr.Contains("php"))
+	fmt.Println("Array contains javascript ?", arrStr.Contains("javascript"))
+	fmt.Println("index of python =", arrStr.Index("python"))
+
+	fmt.Println("Delete index 5 =", arrStr.Delete(5))
+	fmt.Println(arrStr.ToString())
+
+	fmt.Println("Pop: ", arrStr.Pop())
+	fmt.Println(arrStr.ToString())
+
+	fmt.Println("UnShift: ", arrStr.UnShift())
+	fmt.Println(arrStr.ToString())
+
+	fmt.Println("Delete Element objectc:", arrStr.DeleteElement("objectc"))
+	fmt.Println(arrStr.ToString())
 }
-
